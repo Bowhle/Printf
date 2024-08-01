64c7e3f14c618fc94c1651b465d6329c3d768680
Samantha Buhle Mkwananzi <157754224+Bowhle@users.noreply.github.com>
2024-08-01 16:03:13 +0200

    fill_long_oct_array.c is updated

--- fill_long_oct_array.c
+++ fill_long_oct_array.c
@@ -3,9 +3,8 @@
 /**
  * fill_long_oct_array - calculates a long octal number
  *
- * @bnr: array where is stored the binary.
- * @oct: array where is stored the octal.
- *
+ * @bnr: an array where the binary is stored
+ * @oct: array where the octal is stored
  * Return: binary array.
  */
 char *fill_long_oct_array(char *bnr, char *oct)
