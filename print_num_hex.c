5352443f90b235abc0f34b15ad62ce72af5bb569
Samantha Buhle Mkwananzi <157754224+Bowhle@users.noreply.github.com>
2024-08-01 16:57:44 +0200

    print_num_hex.c Update 3

--- print_num_hex.c
+++ print_num_hex.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prinnhex - prints number in hexadecimal starting with zero
+ * prinnhex - prints number in hexadecimal starting with 0x
  * @arguments: input number
  * @buf: buffer pointer
  * @ibuf: index for a buffer pointer
@@ -17,14 +17,14 @@ int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 
 	if (int_input == 0)
 	{
-	ibuf = handl_buf(buf, '0', ibuf);
-	return (1);
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
 	}
 
 	if (int_input < 0)
 	{
-	int_input = (int_input * -1) - 1;
-	isnegative = 1;
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
 	}
 
 	ibuf = handl_buf(buf, '0', ibuf);
@@ -32,18 +32,30 @@ int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 
 	binary = malloc(sizeof(char) * 33); /* 32 bits + 1 for null terminator */
 	if (binary == NULL)
-	return (0);
+		return (0);
 
 	hexadecimal = malloc(sizeof(char) * 9); /* 8 digits + 1 for null terminator */
 	if (hexadecimal == NULL)
 	{
+		free(binary);
+		return (0);
+	}
+
+	fill_binary_array(binary, int_input, isnegative, 32);
+	fill_hex_array(binary, hexadecimal, 0, 8);
+
+	for (first_digit = count = 0, i = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+
 	free(binary);
-	return (0);
+	free(hexadecimal);
+	return (count + 2); /* Include the '0x' prefix in the count */
 }
-
-fill_binary_array(binary, int_input, isnegative, 32);
-fill_hex_array(binary, hexadecimal, 0, 8);
-
-for (first_digit = count = 0, i = 0; hexadecimal[i]; i++)
-{
-if (hexadecimal[i] != '0' && first_digit == 0)
