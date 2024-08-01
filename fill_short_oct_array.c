9207747385a81b7f26e9a67a1a0c46c217c44226
Samantha Buhle Mkwananzi <157754224+Bowhle@users.noreply.github.com>
2024-08-01 16:06:09 +0200

    Update fill_short_oct_array.c

--- fill_short_oct_array.c
+++ fill_short_oct_array.c
@@ -2,11 +2,9 @@
 
 /**
  * fill_short_oct_array - calculates a short octal number
- *
- * @bnr: array where is stored the binary.
- * @oct: array where is stored the octal.
- *
- * Return: binary array.
+ * @bnr: an array where the binary is stored
+ * @oct: an array where the octal is stored 
+ * Return: binary array
  */
 char *fill_short_oct_array(char *bnr, char *oct)
 {
