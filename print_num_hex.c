09ed9241384a49052feaf0492c7956ad8f39bb04
Samantha Buhle Mkwananzi <157754224+Bowhle@users.noreply.github.com>
2024-08-01 16:38:41 +0200

    Update print_num_hex.c

--- print_num_hex.c
+++ print_num_hex.c
@@ -1,46 +1,49 @@
 #include "main.h"
 
 /**
- * prinnhex - print number in hex begining with zero
- * @arguments: input string
+ * prinnhex - prints number in hexadecimal starting with zero
+ * @arguments: input number
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * @ibuf: index for a buffer pointer
  * Return: number of chars printed
  */
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int int_input, i, isnegative, count, first_digit;
+	int int_input, isnegative, count, first_digit, i;
 	char *hexadecimal, *binary;
 
 	int_input = va_arg(arguments, int);
 	isnegative = 0;
+
 	if (int_input == 0)
 	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
+	ibuf = handl_buf(buf, '0', ibuf);
+	return (1);
 	}
+
 	if (int_input < 0)
 	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
+	int_input = (int_input * -1) - 1;
+	isnegative = 1;
 	}
+
 	ibuf = handl_buf(buf, '0', ibuf);
 	ibuf = handl_buf(buf, 'x', ibuf);
-	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
-	hexadecimal = malloc(sizeof(char) * (8 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 8);
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+
+	binary = malloc(sizeof(char) * 33); /* 32 bits + 1 for null terminator */
+	if (binary == NULL)
+	return (0);
+
+	hexadecimal = malloc(sizeof(char) * 9); /* 8 digits + 1 for null terminator */
+	if (hexadecimal == NULL)
 	{
-		if (hexadecimal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
-			count++;
-		}
-	}
 	free(binary);
-	free(hexadecimal);
-	return (count + 2);
+	return (0);
 }
+
+fill_binary_array(binary, int_input, isnegative, 32);
+fill_hex_array(binary, hexadecimal, 0, 8);
+
+for (first_digit = count = 0, i = 0; hexadecimal[i]; i++)
+{
+if (hexadecimal[i] != '0' && first_digit == 0)
